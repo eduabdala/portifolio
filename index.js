13a73cf2e6a915b84dda4db729202630911653ea
Eduardo Abdala <eduardosilvaabdala8@gmail.com>
2023-05-29 21:16:25 +0000

    menu quebrado

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const menuIcon = document.querySelector('.menu-icon');
+  const menu = document.querySelector('.menu');
+
+  menuIcon.addEventListener('click', function() {
+    menu.classList.toggle('active');
+  });
+});
